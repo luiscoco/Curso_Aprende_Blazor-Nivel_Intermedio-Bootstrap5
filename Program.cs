b787cf7314b6316f9da9d0b67d060a3408d263de
Luis Coco Enríquez <luiscocoenriquez@hotmail.com>
2024-09-26 12:02:36 +0200

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+using bootstrap5Samples.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Enable detailed errors in Blazor Server circuits
+builder.Services.AddServerSideBlazor().AddCircuitOptions(options =>
+{
+    options.DetailedErrors = true;
+});
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
